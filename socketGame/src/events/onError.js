4ab34b442d97ad8ead6942f9835d9b7a5496e2ef
Youmin Lee <youmin0107@naver.com>
2025-01-17 01:39:02 +0900

    comment:주석 수정

--- socketGame/src/events/onError.js
+++ socketGame/src/events/onError.js
@@ -4,5 +4,5 @@ export const onError = (socket) => (err) => {
   console.error('소켓 오류:', err);
   // 소켓을 세션에서 제거
   removeUser(socket);
-  console.error('소켓 오류에서 제거',err);
+  console.error('소켓 오류. 소켓 제거',err);
 };
